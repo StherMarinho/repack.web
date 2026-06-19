7d809926c2da18de2abd65ae181ef1ad6e1f01cc
Ana Giulia Ferreira de Abreu Abreu <1394869@sga.pucminas.br>
2026-06-19 17:31:13 -0300

    ajustes envios funcionário

--- src/pages/EnviosAdmin/EnviosAdmin.js
+++ src/pages/EnviosAdmin/EnviosAdmin.js
@@ -20,7 +20,7 @@ const EnviosAdmin = () => {
         const carregar = async () => {
             try {
                 const [resultadoEnvios, resultadoEmpresas] = await Promise.all([
-                    envioService.listarEnvios({}),
+                    envioService.listarEnvios(),
                     empresaService.listarEmpresas()
                 ]);
                     console.log("ENVIOS:", resultadoEnvios);
